e7bc430dcb5966de30b5c464d029174f78270fc3
Leela Krishna KV <leelakrishna2091997@gmail.com>
2024-03-08 13:00:12 -0500

    GraphQL model creation, database connection to the client and GraphQL crud operation calls, AWS Amplify model data validation.

--- App.js
+++ App.js
@@ -2,84 +2,99 @@ import { Amplify } from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
 
+
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  SafeAreaView
+} from 'react-native';
+import { generateClient } from 'aws-amplify/api';
+import { createTodo } from './src/graphql/mutations';
+import { listTodos } from './src/graphql/queries';
 import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, TextInput, StyleSheet, Text, View } from 'react-native';
-// import {React, useState} from 'react';
-import React, { useState } from 'react';
 // App.js
 
 
 
+const initialState = { name: '', description: '' };
+const client = generateClient();
 // import { View, Text, Button } from 'react-native'
 
 export default function App() {
-  const [text, onChangeText] = useState('Useless Text');
-  // const [diceRolls, setDiceRolls] = useState("The saved content");
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
 
-  // const [number, onChangeNumber] = React.useState('');
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await client.graphql({
+        query: listTodos
+      });
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log('error fetching todos');
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await client.graphql({
+        query: createTodo,
+        variables: {
+          input: todo
+        }
+      });
+    } catch (err) {
+      console.log('error creating todo:', err);
+    }
+  }
 
   return (
-
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>My Header</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={(value) => setInput('name', value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <TextInput
+          onChangeText={(value) => setInput('description', value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <Pressable onPress={addTodo} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Create todo</Text>
+        </Pressable>
+        {todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text style={styles.todoDescription}>{todo.description}</Text>
+          </View>
+        ))}
       </View>
-      <View style={styles.contentBox}>
-        <Text style={styles.contentText}>
-          This is some text inside a box. You can customize this paragraph to fit your content needs. This example demonstrates how to align and style components in React Native.
-        </Text>
-      </View>
-    </View>
-
-    // <SafeAreaView>
-    //   <TextInput
-    //     style={styles.input}
-    //     onChangeText={onChangeText}
-    //     value={text}
-    //   />
-    // </SafeAreaView>
+    </SafeAreaView>
   );
 
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>App is working fine...{diceRolls}</Text>
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    height: 60,
-    width: '100%',
-    backgroundColor: '#007bff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerText: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  contentBox: {
-    marginTop: 20,
-    padding: 20,
-    borderWidth: 1,
-    borderColor: '#007bff',
-    borderRadius: 5,
-    backgroundColor: '#e9f5ff',
-  },
-  contentText: {
-    fontSize: 16,
-    color: '#333',
-  },
-});
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
@@ -87,4 +102,47 @@ const styles = StyleSheet.create({
 //     alignItems: 'center',
 //     justifyContent: 'center',
 //   },
+//   header: {
+//     height: 60,
+//     width: '100%',
+//     backgroundColor: '#007bff',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   headerText: {
+//     color: '#fff',
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   contentBox: {
+//     marginTop: 20,
+//     padding: 20,
+//     borderWidth: 1,
+//     borderColor: '#007bff',
+//     borderRadius: 5,
+//     backgroundColor: '#e9f5ff',
+//   },
+//   contentText: {
+//     fontSize: 16,
+//     color: '#333',
+//   },
 // });
+
+const styles = StyleSheet.create({
+  container: { width: 400, flex: 1, padding: 20, alignSelf: 'center' },
+  todo: { marginBottom: 15 },
+  input: {
+    backgroundColor: '#ddd',
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18
+  },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  buttonContainer: {
+    alignSelf: 'center',
+    backgroundColor: 'black',
+    paddingHorizontal: 8
+  },
+  buttonText: { color: 'white', padding: 16, fontSize: 18 }
+});
+
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "everywherebackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
